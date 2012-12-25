548c6ae38510f50d9df2108fb1f2b85ba301d736
Steve Buzonas <steve@fancyguy.com>
2012-12-25 02:03:22 -0500

    Changed approach to override inflector and added test cases to handle new settings.

--- src/Composer/Installers/WordPressInstaller.php
+++ src/Composer/Installers/WordPressInstaller.php
@@ -3,28 +3,22 @@ namespace Composer\Installers;
 
 class WordPressInstaller extends BaseInstaller {
     
-    protected $default_locations = array(
-        'plugin'    => '{$wp-plugins}/{$name}/',
-        'theme'     => '${wp-themes}/{$name}/',
+    protected $locations = array(
+        'plugin'    => '{$wpplugins}/{$name}/',
+        'theme'     => '{$wpthemes}/{$name}/',
     );
     
-    protected $locations = array();
-    
     /**
-     * {@inheritDoc}
+     * Inject wp-content, plugin, and theme extras into path
      */
-    public function getInstallPath(PackageInterface $package, $frameworkType = '') {
-        $extra = $package->getExtra();
+    public function inflectPackageVars($vars) {
+        $extra = $this->package->getExtra();
         
-        $wp_content = !empty($extra['wpcontent_path']) ? $extra['wpcontent_path'] : 'wp-content';
+        $wp_content = !empty($extra['wpcontent-path']) ? $extra['wpcontent-path'] : 'wp-content';
         
-        // check for a custom plugin or theme path
-        $plugin_path = !empty($extra['wpplugin_path']) ? $extra['wpplugin_path'] : $wp_content . '/plugins';
-        $themes_path = !empty($extra['wptheme_path']) ? $extra['wptheme_path'] : $wp_content . '/themes';
+        $vars['wpplugins'] = !empty($extra['wpplugins-path']) ? $extra['wpplugins-path'] : $wp_content . '/plugins';
+        $vars['wpthemes'] = !empty($extra['wpthemes-path']) ? $extra['wpthemes-path'] : $wp_content . '/themes';
         
-        $this->locations['plugin'] = str_replace('{$wp-plugins}', $plugin_path, $this->default_locations['plugin']);
-        $this->locations['theme'] = str_replace('{$wp-themes}', $themes_path, $this->default_locations['theme']);
-        
-        return parent::getInstallPath($package, $frameworkType);
+        return $vars;
     }
 }
