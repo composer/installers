8cdb56f337442685eead744fe8766842f81db3c2
Steve Buzonas <steve@fancyguy.com>
2012-12-25 01:28:05 -0500

    Updated WordPress installer to have extra parameters for wp-content and plugins/themes for custom content directory configurations.

--- src/Composer/Installers/WordPressInstaller.php
+++ src/Composer/Installers/WordPressInstaller.php
@@ -1,10 +1,30 @@
 <?php
 namespace Composer\Installers;
 
-class WordPressInstaller extends BaseInstaller
-{
-    protected $locations = array(
-        'plugin'    => 'wp-content/plugins/{$name}/',
-        'theme'     => 'wp-content/themes/{$name}/',
+class WordPressInstaller extends BaseInstaller {
+    
+    protected $default_locations = array(
+        'plugin'    => '{$wp-plugins}/{$name}/',
+        'theme'     => '${wp-themes}/{$name}/',
     );
+    
+    protected $locations = array();
+    
+    /**
+     * {@inheritDoc}
+     */
+    public function getInstallPath(PackageInterface $package, $frameworkType = '') {
+        $extra = $package->getExtra();
+        
+        $wp_content = !empty($extra['wpcontent_path']) ? $extra['wpcontent_path'] : 'wp-content';
+        
+        // check for a custom plugin or theme path
+        $plugin_path = !empty($extra['wpplugin_path']) ? $extra['wpplugin_path'] : $wp_content . '/plugins';
+        $themes_path = !empty($extra['wptheme_path']) ? $extra['wptheme_path'] : $wp_content . '/themes';
+        
+        $this->locations['plugin'] = str_replace('{$wp-plugins}', $plugin_path, $this->default_locations['plugin']);
+        $this->locations['theme'] = str_replace('{$wp-themes}', $themes_path, $this->default_locations['theme']);
+        
+        return parent::getInstallPath($package, $frameworkType);
+    }
 }
