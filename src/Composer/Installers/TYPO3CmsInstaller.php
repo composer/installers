0cb4d7e09d1a03a65a6055cf84b9f47f70a26621
dkd-egerer Sascha Egerer <sascha.egerer@dkd.de>
2013-02-23 14:24:36 +0100

    Added function to create own IO Object which is passed to the downloader to log stuff

--- src/Composer/Installers/TYPO3CmsInstaller.php
+++ src/Composer/Installers/TYPO3CmsInstaller.php
@@ -1,6 +1,13 @@
 <?php
 namespace Composer\Installers;
 
+use Composer\IO\ConsoleIO;
+use Symfony\Component\Console\Helper\HelperSet;
+use Symfony\Component\Console\Helper\FormatterHelper;
+use Symfony\Component\Console\Helper\DialogHelper;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Input\ArgvInput;
+
 /**
  * Extension installer for TYPO3 CMS
  *
@@ -21,7 +28,28 @@ class TYPO3CmsInstaller extends BaseInstaller {
 	public function __construct(\Composer\Package\PackageInterface $package, \Composer\Composer $composer) {
 		parent::__construct($package, $composer);
 
-		$composer->getDownloadManager()->setDownloader('t3x', new \Composer\Downloader\T3xDownloader(new \Composer\IO\NullIO(),$composer->getConfig()));
+		$io = $this->createIOObject();
+
+		$composer->getDownloadManager()->setDownloader('t3x', new \Composer\Downloader\T3xDownloader($io,$composer->getConfig()));
+	}
+
+	/**
+	 * Currently it is not possible to get the "IO" Object at this point
+	 * so we create our own IO object because the downloader needs it
+	 * to log stuff to the console.
+	 *
+	 * @return ConsoleIO
+	 */
+	private function createIOObject() {
+		$input = new ArgvInput();
+		$output = new ConsoleOutput();
+
+		$helperSet = new HelperSet(array(
+			new FormatterHelper(),
+			new DialogHelper(),
+		));
+
+		return new ConsoleIO($input, $output, $helperSet);
 	}
 
 	/**
