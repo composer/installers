2c9e99a6b92d13f997c9035bb6b9176d176514e7
= <vysinsky@live.com>
2013-05-04 14:44:14 +0200

    Added AnnotateCmsInstaller for support AnnotateCms types

---
+++ src/Composer/Installers/AnnotateCmsInstaller.php
@@ -0,0 +1,14 @@
+<?php
+namespace Composer\Installers;
+
+
+class AnnotateCmsInstaller extends BaseInstaller
+{
+
+    protected $locations = array(
+        'module'    => 'addons/modules/{$name}/',
+        'component' => 'addons/components/{$name}',
+        'service'   => 'addons/services/{$name}',
+    );
+
+}
