d792632d27e1980d921cbbba12eab7b888338d4c
John Huh <me@johnyhuh.com>
2013-10-03 20:57:54 -0700

    Added module for ZF2. Closes GH-106.

--- src/Composer/Installers/ZendInstaller.php
+++ src/Composer/Installers/ZendInstaller.php
@@ -6,5 +6,6 @@ class ZendInstaller extends BaseInstaller
     protected $locations = array(
         'library' => 'library/{$name}/',
         'extra'   => 'extras/library/{$name}/',
+        'module'  => 'module/{$name}/',
     );
 }
