ad0f8a64bfaec7628ca4a1a24355ac66fd52425f
sagikazarmark <mark.sagikazar@gmail.com>
2013-12-30 21:40:36 +0100

    Added theme support for FuelInstaller
    Please note that there is no default location for themes in FuelPHP 1.x, but this path is mentioned as a possible default in the docs. Make sure to change the config. (Actually there is a default, but it is in the DOCROOT, which is not recommended)

--- src/Composer/Installers/FuelInstaller.php
+++ src/Composer/Installers/FuelInstaller.php
@@ -6,5 +6,6 @@ class FuelInstaller extends BaseInstaller
     protected $locations = array(
         'module'  => 'fuel/app/modules/{$name}/',
         'package' => 'fuel/packages/{$name}/',
+        'theme'   => 'fuel/themes/{$name}/',
     );
 }
