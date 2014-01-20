4e870a6425a9189766d90fcb5ad328e50121c657
Drak <drak@zikula.org>
2014-01-20 19:31:58 +0000

    Add ZikulaInstaller to Installer types

--- src/Composer/Installers/Installer.php
+++ src/Composer/Installers/Installer.php
@@ -40,6 +40,7 @@ class Installer extends LibraryInstaller
         'symfony1'     => 'Symfony1Installer',
         'wordpress'    => 'WordPressInstaller',
         'zend'         => 'ZendInstaller',
+        'zikula'       => 'ZikulaInstaller',
         'typo3-flow'   => 'TYPO3FlowInstaller',
         'typo3-cms'    => 'TYPO3CmsInstaller',
     );
