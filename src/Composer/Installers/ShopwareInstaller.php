441ede1551c2fc132125677965fc2c0476d23163
Benjamin Boit <b.boit@posteo.de>
2014-01-27 16:14:59 +0000

    fixed plugin-naming

--- src/Composer/Installers/ShopwareInstaller.php
+++ src/Composer/Installers/ShopwareInstaller.php
@@ -1,6 +1,10 @@
 <?php
 namespace Composer\Installers;
 
+/**
+ * Plugin/theme installer for shopware
+ * @author Benjamin Boit
+ */
 class ShopwareInstaller extends BaseInstaller
 {
     protected $locations = array(
@@ -9,4 +13,46 @@ class ShopwareInstaller extends BaseInstaller
         'frontend-plugin'   => 'engine/Shopware/Plugins/Local/Frontend/{$name}/',
         'theme'             => 'templates/{$name}/'
     );
+
+    /**
+     * Transforms the names
+     * @param array $vars
+     * @return array
+     */
+    public function inflectPackageVars($vars)
+    {
+        if($vars['type'] === 'shopware-theme') {
+            return $this->correctThemeName($vars);
+        } else {
+            return $this->correctPluginName($vars);
+        }
+    }
+
+    /**
+     * Changes the name to a camelcased combination of vendor and name
+     * @param array $vars
+     * @return array
+     */
+    private function correctPluginName($vars)
+    {
+        $camelCasedName = preg_replace_callback('/(-[a-z])/', function ($matches) {
+            return strtoupper($matches[0][1]);
+        }, $vars['name']);
+
+        $vars['name'] = ucfirst($vars['vendor']) . ucfirst($camelCasedName);
+
+        return $vars;
+    }
+
+    /**
+     * Changes the name to a underscore separated name
+     * @param array $vars
+     * @return array
+     */
+    private function correctThemeName($vars)
+    {
+        $vars['name'] = str_replace('-', '_', $vars['name']);
+
+        return $vars;
+    }
 }
