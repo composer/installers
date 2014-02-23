f31842ef254ffa89ec963de41cd386285747170d
Martijn <martijn.niji@gmail.com>
2014-02-23 14:53:42 +0100

    Finalize Wolf CMS installer

--- src/Composer/Installers/WolfCMSInstaller.php
+++ src/Composer/Installers/WolfCMSInstaller.php
@@ -1,11 +1,9 @@
 <?php
 namespace Composer\Installers;
 
-class WordPressInstaller extends BaseInstaller
+class WolfCMSInstaller extends BaseInstaller
 {
     protected $locations = array(
-        'plugin'    => 'wp-content/plugins/{$name}/',
-        'theme'     => 'wp-content/themes/{$name}/',
-        'muplugin'  => 'wp-content/mu-plugins/{$name}/',
+        'plugin'    => 'wolf/plugins/{$name}/',
     );
 }
