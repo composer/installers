0ecd49f48a919105968023489e1acb9bc572cc35
Stephan Gambke <s7eph4n@gmail.com>
2014-04-03 20:36:46 +0000

    Add mediawiki-skin package type, modify mediawiki-extension package type
    Name inflection for mediawiki-extension stays the same, except that a trailing
    '-extension' is cut off if present.
    
    Name inflection for mediawiki-skin will just cut off a trailing '-skin' if present.
    
    That way package name collisions, e.g. between extension Bootstrap and skin
    Bootstrap (they actually do exist both!), can be avoided.
    
    The installation directory for skins is 'skins'.

--- src/Composer/Installers/MediaWikiInstaller.php
+++ src/Composer/Installers/MediaWikiInstaller.php
@@ -5,15 +5,33 @@ class MediaWikiInstaller extends BaseInstaller
 {
     protected $locations = array(
         'extension' => 'extensions/{$name}/',
+        'skin' => 'skins/{$name}/',
     );
 
     /**
-     * Format package name to CamelCase keeping existing uppercase chars.
+     * Format package name.
+     *
+     * For package type mediawiki-extension, cut off a trailing '-extension' if present and transform
+     * to CamelCase keeping existing uppercase chars.
+     *
+     * For package type mediawiki-skin, cut off a trailing '-skin' if present.
+     *
      */
     public function inflectPackageVars($vars)
     {
-        $vars['name'] = str_replace('-', ' ', $vars['name']);
-        $vars['name'] = str_replace(' ', '', ucwords($vars['name']));
+
+        switch ( $vars['type'] ) {
+
+            case 'mediawiki-extension':
+                $vars['name'] = preg_replace( '/-extension$/', '', $vars['name']);
+                $vars['name'] = str_replace('-', ' ', $vars['name']);
+                $vars['name'] = str_replace(' ', '', ucwords($vars['name']));
+                break;
+
+            case 'mediawiki-skin':
+                $vars['name'] = preg_replace( '/-skin$/', '', $vars['name']);
+                break;
+        }
 
         return $vars;
     }
