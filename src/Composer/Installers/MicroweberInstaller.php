8aaf3c542deade4e4e2cb3a1282a2a5a5f0d16d3
Peter Ivanov <peter@microweber.com>
2014-05-03 17:56:13 +0300

    test

--- src/Composer/Installers/MicroweberInstaller.php
+++ src/Composer/Installers/MicroweberInstaller.php
@@ -5,6 +5,7 @@ class MicroweberInstaller extends BaseInstaller
 {
     protected $locations = array(
         'module'    => 'userfiles/modules/{$name}/',
+        'core'    => 'src/Microweber/{$name}',
         'template'     => 'userfiles/templates/{$name}/',
         'elements'  => 'userfiles/elements/{$name}/',
     );
