d19a5e10974782e654db1100799e02c1f95683b9
Joshua Estes <Joshua@Estes.in>
2014-05-22 17:40:57 -0500

    Some spaces need to go

--- src/Composer/Installers/PuppetInstaller.php
+++ src/Composer/Installers/PuppetInstaller.php
@@ -4,7 +4,7 @@ namespace Composer\Installers;
 class PuppetInstaller extends BaseInstaller
 {
     protected $locations = array(
-        'module'  => 'modules/{$name}/',
+        'module' => 'modules/{$name}/',
     );
 }
 
