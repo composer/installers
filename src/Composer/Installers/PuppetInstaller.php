fff573b7b3689768773ed949599980c79a7e4d7c
Joshua Estes <Joshua@Estes.in>
2014-05-22 17:33:11 -0500

    A few updates for bug fixes?

--- src/Composer/Installers/PuppetInstaller.php
+++ src/Composer/Installers/PuppetInstaller.php
@@ -1,11 +1,10 @@
 <?php
-
 namespace Composer\Installers;
 
 class PuppetInstaller extends BaseInstaller
 {
     protected $locations = array(
-        'module'  => 'module/{$name}/',
+        'module'  => 'modules/{$name}/',
     );
 }
 
