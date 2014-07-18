6f9318220bfbf260f545e8dbf22605bdcbe5b481
Dave Holloway <dh@gn2-netwerk.de>
2014-07-18 16:05:11 +0200

    Redaxo be_style plugin & unit test

--- src/Composer/Installers/RedaxoInstaller.php
+++ src/Composer/Installers/RedaxoInstaller.php
@@ -4,6 +4,7 @@ namespace Composer\Installers;
 class RedaxoInstaller extends BaseInstaller
 {
     protected $locations = array(
-        'addon'    => 'redaxo/include/addons/{$name}/',
+        'addon'          => 'redaxo/include/addons/{$name}/',
+        'bestyle-plugin' => 'redaxo/include/addons/be_style/plugins/{$name}/'
     );
 }
