88a6d8461c121859b68feec0b2996192d495ddc4
tstoeckler <tobiasstoeckler@googlemail.com>
2014-08-18 03:26:58 +0200

    Support Drupal core repositories.
    This provides support for Drupal core repositories such as https://github.com/tstoeckler/drupal-core

--- src/Composer/Installers/DrupalInstaller.php
+++ src/Composer/Installers/DrupalInstaller.php
@@ -6,8 +6,9 @@ class DrupalInstaller extends BaseInstaller
     protected $locations = array(
         'module'    => 'modules/{$name}/',
         'theme'     => 'themes/{$name}/',
-        'library'     => 'libraries/{$name}/',
+        'library'   => 'libraries/{$name}/',
         'profile'   => 'profiles/{$name}/',
         'drush'     => 'drush/{$name}/',
+        'core'      => 'core',
     );
 }
