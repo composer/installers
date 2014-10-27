864c6eb217564b05df6e7ad3eda9bdfffb931dd1
Christian Loock <chl@vkf-renzel.de>
2014-10-27 10:57:53 +0100

    Added type oxid-shop

--- src/Composer/Installers/OxidInstaller.php
+++ src/Composer/Installers/OxidInstaller.php
@@ -4,8 +4,9 @@ namespace Composer\Installers;
 class OxidInstaller extends BaseInstaller
 {
     protected $locations = array(
+	'shop'		=> '{$name}/',
         'module'    => 'modules/{$name}/',
         'theme'  => 'application/views/{$name}/',
-        'out'    => 'out/{$name}/',
+        'out'    => 'out/{$name}/'
     );
 }
