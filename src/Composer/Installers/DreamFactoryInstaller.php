877e9c50855631b39cdfba5c3238b09659341aa8
Jerry Ablan <jerryablan@dreamfactory.com>
2015-01-21 16:25:20 -0500

    Check version directory change

--- src/Composer/Installers/DreamFactoryInstaller.php
+++ src/Composer/Installers/DreamFactoryInstaller.php
@@ -3,9 +3,44 @@ namespace Composer\Installers;
 
 class DreamFactoryInstaller extends BaseInstaller
 {
+    /**
+     * @type array This installer's paths
+     */
     protected $locations = array(
         'plugin'  => 'storage/plugins/{$vendor}/{$name}/',
         'library' => 'storage/plugins/{$vendor}/{$name}/',
+        //  DSP v1.x
         'app'     => 'web/{$name}/',
     );
+
+    /**
+     * DSP v1 uses "web" for HTML. DSP v2 will use "public". This
+     * method checks to see which version we have and install to
+     * the proper directory.
+     *
+     * @return array
+     */
+    public function getLocations()
+    {
+        static $_cwd = null;
+
+        $_paths = array(
+            'public',
+            'web',
+        );
+
+        $_cwd = $_cwd ?: getcwd();
+
+        foreach ( $_paths as $_path )
+        {
+            if ( is_dir( $_cwd . '/' . $_path ) )
+            {
+                $this->locations['app'] = $_path . '/{$name}/';
+                break;
+            }
+        }
+
+        return parent::getLocations();
+    }
+
 }
