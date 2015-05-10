63f696a66a06230d41814a84b8155f1497de4337
Denis Kulichkin <onexhovia@gmail.com>
2015-05-10 12:27:29 +0300

    Change locations path with "local" on "bitrix"

--- src/Composer/Installers/BitrixInstaller.php
+++ src/Composer/Installers/BitrixInstaller.php
@@ -4,8 +4,8 @@ namespace Composer\Installers;
 class BitrixInstaller extends BaseInstaller
 {
     protected $locations = array(
-        'module'    => 'local/modules/{$name}/',
-        'component' => 'local/components/{$name}/',
-        'theme'     => 'local/templates/{$name}/'
+        'module'    => 'bitrix/modules/{$name}/',
+        'component' => 'bitrix/components/{$name}/',
+        'theme'     => 'bitrix/templates/{$name}/'
     );
 }
