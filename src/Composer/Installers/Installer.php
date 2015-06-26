d86453dac378f8549bd79e1f57d64c4f3e3a059c
Hieu Nguyen <hieunhit@gmail.com>
2015-06-27 00:42:28 +0700

    add HnhInstaller

--- src/Composer/Installers/Installer.php
+++ src/Composer/Installers/Installer.php
@@ -34,6 +34,7 @@ class Installer extends LibraryInstaller
         'fuel'         => 'FuelInstaller',
         'fuelphp'      => 'FuelphpInstaller',
         'grav'         => 'GravInstaller',
+        'hnh'          => 'HnhInstaller',
         'hurad'        => 'HuradInstaller',
         'joomla'       => 'JoomlaInstaller',
         'kirby'        => 'KirbyInstaller',
