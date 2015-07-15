5266dffd0b792544163d8b9d2434eb73d1239278
Tobias Johannes <tobias.johannes@dmk-ebusiness.de>
2015-07-15 17:40:58 +0200

    humhub installer: remove not used names

--- src/Composer/Installers/HumHubInstaller.php
+++ src/Composer/Installers/HumHubInstaller.php
@@ -4,44 +4,42 @@ namespace Composer\Installers;
 class HumHubInstaller extends BaseInstaller
 {
     protected $locations = array(
-        'module'    => 'humhub/modules/{$name}/',
-        'theme'     => 'humhub/themes/{$name}/'
+        'module'    => 'modules/{$name}/',
+        'theme'     => 'themes/{$name}/'
     );
 
     /**
      * Format package name.
      *
-     * For package type october-plugin, cut off a trailing '-plugin' if present.
+     * For package type humhub-module, cut off 'humhub-module-' if present.
      *
-     * For package type october-theme, cut off a trailing '-theme' if present.
+     * For package type humhub-theme, cut off 'humhub-theme-' if present.
      *
      */
-	 /*
     public function inflectPackageVars($vars)
     {
-        if ($vars['type'] === 'october-plugin') {
-            return $this->inflectPluginVars($vars);
+        if ($vars['type'] === 'humhub-module') {
+            return $this->inflectModuleVars($vars);
         }
 
-        if ($vars['type'] === 'october-theme') {
+        if ($vars['type'] === 'humhub-theme') {
             return $this->inflectThemeVars($vars);
         }
 
         return $vars;
     }
 
-    protected function inflectPluginVars($vars)
+    protected function inflectModuleVars($vars)
     {
-        $vars['name'] = preg_replace('/-plugin$/', '', $vars['name']);
+        $vars['name'] = preg_replace('/humhub-module-$/', '', $vars['name']);
 
         return $vars;
     }
 
     protected function inflectThemeVars($vars)
     {
-        $vars['name'] = preg_replace('/-theme$/', '', $vars['name']);
+        $vars['name'] = preg_replace('/humhub-theme-$/', '', $vars['name']);
 
         return $vars;
     }
-	*/
 }
