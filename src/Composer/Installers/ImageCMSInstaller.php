7cfcffbbe54032517c787440e862ec499ccff484
gulaandrij <gyla.andrij@gmail.com>
2015-08-27 23:34:46 +0300

    add ImageCMS installers

--- src/Composer/Installers/ImageCMSInstaller.php
+++ src/Composer/Installers/ImageCMSInstaller.php
@@ -5,7 +5,7 @@ class ImageCMSInstaller extends BaseInstaller
 {
     protected $locations = array(
         'template'    => 'templates/{$name}/',
-        'module'     => 'application/modules/{$name}/',
+        'module'      => 'application/modules/{$name}/',
         'library'     => 'application/libraries/{$name}/',
     );
 }
