1d518e27bad1226a2e7c170d0c462c1498f335d9
Sascha Marcel Schmidt <mail@saschaschmidt.net>
2015-10-09 14:10:26 +0200

    testing install path value

--- src/Composer/Installers/OxidInstaller.php
+++ src/Composer/Installers/OxidInstaller.php
@@ -8,4 +8,32 @@ class OxidInstaller extends BaseInstaller
         'theme'  => 'application/views/{$name}/',
         'out'    => 'out/{$name}/',
     );
+
+	/**
+	 * getInstallPath
+	 *
+	 * @param PackageInterface $package
+	 * @param string $frameworkType
+	 * @return void
+	 */
+	public function getInstallPath(PackageInterface $package, $frameworkType = '')
+	{
+		$installPath = parent::getInstallPath($package, $frameworkType);
+		$this->prepareVendorDirectory($installPath);
+		return $installPath;
+	}
+
+	/**
+	 * prepareVendorDirectory
+	 *
+	 * Makes sure there is a vendormetadata.php file inside
+	 * the vendor folder if there is a vendor folder.
+	 *
+	 * @param string $installPath
+	 * @return void
+	 */
+	protected function prepareVendorDirectory($installPath)
+	{
+		echo $installPath;
+	}
 }
