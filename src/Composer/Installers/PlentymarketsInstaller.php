b98593691449ccb90f3b3eca171c532cd40e6055
ChristianMoll <christian.moll@plentymarkets.com>
2016-08-12 10:28:24 +0200

    Adjusted code style
    Changed code style to fit PSR-2 style guide.

--- src/Composer/Installers/PlentymarketsInstaller.php
+++ src/Composer/Installers/PlentymarketsInstaller.php
@@ -9,20 +9,21 @@ class PlentymarketsInstaller extends BaseInstaller
 
     /**
      * Remove hyphen, "plugin" and format to camelcase
+     * @param array $vars
+     *
+     * @return array
      */
     public function inflectPackageVars($vars)
     {
-      $vars['name'] = explode("-", $vars['name']);
-      foreach($vars['name'] as $key => $name)
-      {
-          $vars['name'][$key] = ucfirst($vars['name'][$key]);
-          if(strcasecmp($name, "Plugin") == 0)
-          {
-              unset($vars['name'][$key]);
-          }
-      }
-      $vars['name'] = implode("",$vars['name']);
+        $vars['name'] = explode("-", $vars['name']);
+        foreach ($vars['name'] as $key => $name) {
+            $vars['name'][$key] = ucfirst($vars['name'][$key]);
+            if (strcasecmp($name, "Plugin") == 0) {
+                unset($vars['name'][$key]);
+            }
+        }
+        $vars['name'] = implode("",$vars['name']);
 
-      return $vars;
+        return $vars;
     }
 }
