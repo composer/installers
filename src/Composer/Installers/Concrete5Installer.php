a8aaf0887fc7a85ed8b189326eed7bb7f2a3ff1f
Korvin Szanto <Korvinszanto@gmail.com>
2017-01-25 08:34:54 -0800

    Add `core` and remove `updates` from concrete5 installer
    This allows us to properly install the core via composer.

--- src/Composer/Installers/Concrete5Installer.php
+++ src/Composer/Installers/Concrete5Installer.php
@@ -4,9 +4,9 @@ namespace Composer\Installers;
 class Concrete5Installer extends BaseInstaller
 {
     protected $locations = array(
+        'core'       => 'concrete/',
         'block'      => 'application/blocks/{$name}/',
         'package'    => 'packages/{$name}/',
         'theme'      => 'application/themes/{$name}/',
-        'update'     => 'updates/{$name}/',
     );
 }
