269ad2941009a52e6aaa35a085a10f26c7a4482d
blackleg <hectorespertpardo@gmail.com>
2017-06-23 17:29:21 +0200

    Add osclass theme and language installer

--- src/Composer/Installers/OsclassInstaller.php
+++ src/Composer/Installers/OsclassInstaller.php
@@ -11,6 +11,8 @@ class OsclassInstaller extends BaseInstaller {
     
     protected $locations = array(
         'plugin' => 'oc-content/plugins/{$name}/',
+        'theme' => 'oc-content/themes/{$name}/',
+        'language' => 'oc-content/languages/{$name}/',
     );
     
 }
