b15f6bd76824d9d89fdecefed65fa18e4f7a0eca
Kadir Yücel <ky@clicktrend.de>
2017-12-26 17:13:48 +0100

    CiviCrm added

--- src/Composer/Installers/CiviCrmInstaller.php
+++ src/Composer/Installers/CiviCrmInstaller.php
@@ -4,6 +4,6 @@ namespace Composer\Installers;
 class CiviCrmInstaller extends BaseInstaller
 {
     protected $locations = array(
-        'module'    => 'ext/{$name}/'
+        'ext'    => 'ext/{$name}/'
     );
 }
