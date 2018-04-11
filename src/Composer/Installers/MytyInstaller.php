3225e8ab62137552b11fa8ee917902787d40b39a
Mike Reiche <mike.reiche@tyclipso.net>
2018-04-11 10:38:31 +0200

    Adder installer for myty modules and projects

---
+++ src/Composer/Installers/MytyInstaller.php
@@ -0,0 +1,18 @@
+<?php
+namespace Composer\Installers;
+
+/**
+ * Composer installer for myty projects and modules.
+ * Myty is a closed source CMF/CMS developed by tyclipso.net since 2003
+ * @see https://myty.readme.io
+ * @see https://tyclipso.net
+ * @author Mike Reiche <mike.reiche@tyclipso.net>
+ */
+class MytyInstaller extends BaseInstaller
+{
+    // Myty composer components are located in DocRoot/3rdParty
+    protected $locations = array(
+        'project' => '../projects/{$name}/',
+        'module'   => '../tycon/modules/{$name}/'
+    );
+}
