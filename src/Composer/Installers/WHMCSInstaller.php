7affdad7cb23bf3cc188357223fd99954d8c661c
Björn Strausmann <bjoern@strausmann.net>
2018-08-07 20:39:11 +0200

    Update WHMCSInstaller.php
    Added more installation locations

--- src/Composer/Installers/WHMCSInstaller.php
+++ src/Composer/Installers/WHMCSInstaller.php
@@ -5,6 +5,22 @@ namespace Composer\Installers;
 class WHMCSInstaller extends BaseInstaller
 {
     protected $locations = array(
-        'gateway' => 'modules/gateways/{$name}/',
+        'addons' => 'modules/addons/{$name}/',
+        'fraud' => 'modules/fraud/{$name}/',
+        'gateways' => 'modules/gateways/{$name}/',
+        'notifications' => 'modules/notifications/{$name}/',
+        'registrars' => 'modules/registrars/{$name}/',
+        'reports' => 'modules/reports/{$name}/',
+        'security' => 'modules/security/{$name}/',
+        'servers' => 'modules/servers/{$name}/',
+        'social' => 'modules/social/{$name}/',
+        'support' => 'modules/support/{$name}/',
+        'widgets' => 'modules/widgets/{$name}/',
+        'hooks' => 'includes/hooks/',
+        'api' => 'includes/api/',
+        'lang' => 'lang/overrides/',
+        'feeds' => 'feeds/',
+        'templates' => 'templates/{$name}/',
+        'includes' => 'includes/{$name}/'
     );
 }
