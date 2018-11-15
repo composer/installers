b591c3cce0571502c979c6b8cbd72cf57dcf9cd3
wsm <nicolas.james.cole@gmail.com>
2018-11-15 10:57:54 +1000

    Add the custom profile path.

--- src/Composer/Installers/DrupalInstaller.php
+++ src/Composer/Installers/DrupalInstaller.php
@@ -4,13 +4,14 @@ namespace Composer\Installers;
 class DrupalInstaller extends BaseInstaller
 {
     protected $locations = array(
-        'core'      => 'core/',
-        'module'    => 'modules/{$name}/',
-        'theme'     => 'themes/{$name}/',
-        'library'   => 'libraries/{$name}/',
-        'profile'   => 'profiles/{$name}/',
-        'drush'     => 'drush/{$name}/',
-	    'custom-theme' => 'themes/custom/{$name}/',
-	    'custom-module' => 'modules/custom/{$name}/',
+        'core'           => 'core/',
+        'module'         => 'modules/{$name}/',
+        'theme'          => 'themes/{$name}/',
+        'library'        => 'libraries/{$name}/',
+        'profile'        => 'profiles/{$name}/',
+        'drush'          => 'drush/{$name}/',
+        'custom-theme'   => 'themes/custom/{$name}/',
+        'custom-module'  => 'modules/custom/{$name}/',
+        'custom-profile' => 'profiles/custom/{$name}/'
     );
 }
