2e4607ccb740af8cd9ef774c48fe466971e21619
GothShoot <34012335+GothShoot@users.noreply.github.com>
2019-01-23 20:11:40 +0100

    Update NitroPHPInstaller.php

--- src/Composer/Installers/NitroPHPInstaller.php
+++ src/Composer/Installers/NitroPHPInstaller.php
@@ -5,6 +5,6 @@ namespace Composer\Installers;
 class NitroPHPInstaller extends BaseInstaller
 {
     protected $locations = array(
-        'module'    => 'modules/{$name}/'
+        'module'    => 'Module/{$name}/'
     );
 }
