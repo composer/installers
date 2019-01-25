5d51c2c61a0a66e312057bb14c74b8a93c9a528f
wsm <swim@users.noreply.github.com>
2019-01-25 17:48:56 +1000

    Drupal custom profile installation path (#416)
    * Add the custom profile path.
    
    * Include the custom Drupal profile test.

--- src/Composer/Installers/DrupalInstaller.php
+++ src/Composer/Installers/DrupalInstaller.php
@@ -4,14 +4,15 @@ namespace Composer\Installers;
 class DrupalInstaller extends BaseInstaller
 {
     protected $locations = array(
-        'core'      => 'core/',
-        'module'    => 'modules/{$name}/',
-        'theme'     => 'themes/{$name}/',
-        'library'   => 'libraries/{$name}/',
-        'profile'   => 'profiles/{$name}/',
-        'drush'     => 'drush/{$name}/',
-	    'custom-theme' => 'themes/custom/{$name}/',
-	    'custom-module' => 'modules/custom/{$name}/',
-        'drupal-multisite' => 'sites/{$name}/',
+        'core'             => 'core/',
+        'module'           => 'modules/{$name}/',
+        'theme'            => 'themes/{$name}/',
+        'library'          => 'libraries/{$name}/',
+        'profile'          => 'profiles/{$name}/',
+        'drush'            => 'drush/{$name}/',
+        'custom-theme'     => 'themes/custom/{$name}/',
+        'custom-module'    => 'modules/custom/{$name}/',
+        'custom-profile'   => 'profiles/custom/{$name}/',
+        'drupal-multisite' => 'sites/{$name}/'
     );
 }
