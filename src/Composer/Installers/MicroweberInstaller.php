d924c1bac31ecd9460a0c8a40570fd785dc2d1e3
Peter Ivanov <peter@microweber.com>
2019-07-10 18:24:09 +0300

    update

--- src/Composer/Installers/MicroweberInstaller.php
+++ src/Composer/Installers/MicroweberInstaller.php
@@ -4,12 +4,12 @@ namespace Composer\Installers;
 class MicroweberInstaller extends BaseInstaller
 {
     protected $locations = array(
-        'module'      => 'userfiles/modules/{$install_item_dir}/',
+        'module' => 'userfiles/modules/{$install_item_dir}/',
         'module-skin' => 'userfiles/modules/{$install_item_dir}/templates/',
-        'template'    => 'userfiles/templates/{$install_item_dir}/',
-        'element'     => 'userfiles/elements/{$install_item_dir}/',
-        'vendor'      => 'vendor/{$install_item_dir}/',
-        'components'  => 'components/{$install_item_dir}/'
+        'template' => 'userfiles/templates/{$install_item_dir}/',
+        'element' => 'userfiles/elements/{$install_item_dir}/',
+        'vendor' => 'vendor/{$install_item_dir}/',
+        'components' => 'components/{$install_item_dir}/'
     );
 
     /**
@@ -23,35 +23,37 @@ class MicroweberInstaller extends BaseInstaller
     public function inflectPackageVars($vars)
     {
 
-        $vars['install_item_dir'] = $vars['name'];
+
         if ($this->package->getTargetDir()) {
             $vars['install_item_dir'] = $this->package->getTargetDir();
+        } else {
+            $vars['install_item_dir'] = $vars['name'];
+            if ($vars['type'] === 'microweber-template') {
+                return $this->inflectTemplateVars($vars);
+            }
+            if ($vars['type'] === 'microweber-templates') {
+                return $this->inflectTemplatesVars($vars);
+            }
+            if ($vars['type'] === 'microweber-core') {
+                return $this->inflectCoreVars($vars);
+            }
+            if ($vars['type'] === 'microweber-adapter') {
+                return $this->inflectCoreVars($vars);
+            }
+            if ($vars['type'] === 'microweber-module') {
+                return $this->inflectModuleVars($vars);
+            }
+            if ($vars['type'] === 'microweber-modules') {
+                return $this->inflectModulesVars($vars);
+            }
+            if ($vars['type'] === 'microweber-skin') {
+                return $this->inflectSkinVars($vars);
+            }
+            if ($vars['type'] === 'microweber-element' or $vars['type'] === 'microweber-elements') {
+                return $this->inflectElementVars($vars);
+            }
         }
 
-        if ($vars['type'] === 'microweber-template') {
-            return $this->inflectTemplateVars($vars);
-        }
-        if ($vars['type'] === 'microweber-templates') {
-            return $this->inflectTemplatesVars($vars);
-        }
-        if ($vars['type'] === 'microweber-core') {
-            return $this->inflectCoreVars($vars);
-        }
-        if ($vars['type'] === 'microweber-adapter') {
-            return $this->inflectCoreVars($vars);
-        }
-        if ($vars['type'] === 'microweber-module') {
-            return $this->inflectModuleVars($vars);
-        }
-        if ($vars['type'] === 'microweber-modules') {
-            return $this->inflectModulesVars($vars);
-        }
-        if ($vars['type'] === 'microweber-skin') {
-            return $this->inflectSkinVars($vars);
-        }
-        if ($vars['type'] === 'microweber-element' or $vars['type'] === 'microweber-elements') {
-            return $this->inflectElementVars($vars);
-        }
 
         return $vars;
     }
