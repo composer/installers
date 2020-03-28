d2a98321ab66945396ef147387f93eee4c05089f
Jordi Boggiano <j.boggiano@seld.be>
2020-03-28 20:44:55 +0100

    Fix support for composer v2

--- src/Composer/Installers/Plugin.php
+++ src/Composer/Installers/Plugin.php
@@ -8,10 +8,20 @@ use Composer\Plugin\PluginInterface;
 
 class Plugin implements PluginInterface
 {
+    private $installer;
 
     public function activate(Composer $composer, IOInterface $io)
     {
-        $installer = new Installer($io, $composer);
-        $composer->getInstallationManager()->addInstaller($installer);
+        $this->installer = new Installer($io, $composer);
+        $composer->getInstallationManager()->addInstaller($this->installer);
+    }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+        $composer->getInstallationManager()->removeInstaller($this->installer);
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
     }
 }
