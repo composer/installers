3de63c2f07e47bc4220352831a321da4c16888f2
Ali Gangji <ali@neonrain.com>
2021-01-10 16:53:52 -0700

    Add additional Starbug installer types

--- src/Composer/Installers/StarbugInstaller.php
+++ src/Composer/Installers/StarbugInstaller.php
@@ -4,6 +4,9 @@ namespace Composer\Installers;
 class StarbugInstaller extends BaseInstaller
 {
     protected $locations = array(
-        'module' => 'modules/{$name}/'
+        'module' => 'modules/{$name}/',
+        'theme' => 'themes/{$name}/',
+        'custom-module' => 'app/modules/{$name}/',
+        'custom-theme' => 'app/themes/{$name}/'
     );
 }
