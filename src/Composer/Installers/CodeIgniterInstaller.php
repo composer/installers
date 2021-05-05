806077ea8fe24abcadf22fdb4964b123d8a6ced1
Adi Prasetyo <3001652+sukrosono@users.noreply.github.com>
2021-05-05 12:16:17 +0700

    add new type

--- src/Composer/Installers/CodeIgniterInstaller.php
+++ src/Composer/Installers/CodeIgniterInstaller.php
@@ -5,6 +5,7 @@ class CodeIgniterInstaller extends BaseInstaller
 {
     protected $locations = array(
         'library'     => 'application/libraries/{$name}/',
+        'helpers' => 'application/helpers/{$name}/',
         'third-party' => 'application/third_party/{$name}/',
         'module'      => 'application/modules/{$name}/',
     );
